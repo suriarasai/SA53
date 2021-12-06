664931dd656ff28cb8ca5c30deec51055c7f5e3d
SURIA <suria.r.asai@gmail.com>
2021-12-06 14:30:26 +0800

    first commit

---
+++ welcome/src/main/java/sg/edu/nus/welcome/WelcomeApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.welcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeApplication.class, args);
+	}
+
+}
