3d5540e249c4be4dceedfb6c586dcf4653d8a2c1
suriarasai <suria.r.asai@gmail.com>
2021-12-07 10:25:53 +0800

    lombok

--- welcome/src/main/java/sg/edu/nus/welcome/model/Customer.java
+++ welcome/src/main/java/sg/edu/nus/welcome/model/Customer.java
@@ -1,68 +1,40 @@
 package sg.edu.nus.welcome.model;
 
-import java.util.Objects;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
 @Table(name="customer")
-public class Customer {
+@Data
+@NoArgsConstructor
+public class Customer implements Comparable<Customer> {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer customerId;
 	private String name;
 	private String address;
-	public Customer() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+	
 	public Customer(String name, String address) {
 		super();
 		this.name = name;
 		this.address = address;
 	}
-	public Integer getCustomerId() {
-		return customerId;
-	}
-	public void setCustomerId(Integer customerId) {
-		this.customerId = customerId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(customerId);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Customer other = (Customer) obj;
-		return Objects.equals(customerId, other.customerId);
-	}
-	@Override
-	public String toString() {
-		return "Customer [customerId=" + customerId + ", name=" + name + ", address=" + address + "]";
-	}
-	
-	
 
+	@Override
+	public int compareTo(Customer o) {
+		// TODO Auto-generated method stub
+		if (this.customerId == o.getCustomerId()) 
+			return 0;
+			else 
+		return -1;
+	}
+	
+	
+	
 }
--- welcome/src/main/java/sg/edu/nus/welcome/model/Product.java
+++ welcome/src/main/java/sg/edu/nus/welcome/model/Product.java
@@ -1,14 +1,17 @@
 package sg.edu.nus.welcome.model;
 
-import java.util.Objects;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
 @Entity
 @Table(name="product")
+@Data
+@NoArgsConstructor
 public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,60 +19,11 @@ public class Product {
 	private String name;
 	private String description;
 	private Double price;
-	public Product() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
 	public Product(String name, String description, Double price) {
 		super();
 		this.name = name;
 		this.description = description;
 		this.price = price;
 	}
-	public Integer getProductId() {
-		return productId;
-	}
-	public void setProductId(Integer productId) {
-		this.productId = productId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public Double getPrice() {
-		return price;
-	}
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(productId);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Product other = (Product) obj;
-		return Objects.equals(productId, other.productId);
-	}
-	@Override
-	public String toString() {
-		return "Product [productId=" + productId + ", name=" + name + ", description=" + description + ", price="
-				+ price + "]";
-	}
-	
 
 }
